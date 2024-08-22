f429ee94ce01378a2e1702fcd4cfe4d14b481b4f
Juan Esteban Castaño <osmajuan760@gmail.com>
2024-08-22 17:24:56 -0500

    union de conjuntos completada

--- taller1/main.py
+++ taller1/main.py
@@ -1,3 +1,13 @@
-#Interseccion de conjuntos
+def union_conjuntos_recursiva(*conjuntos: list, pos: int, conjunto_aux: list) -> None:
 
-d
+    if pos > conjuntos.count:
+        return conjunto_aux
+
+    return union_conjuntos_recursiva(conjuntos, pos+1, union_conjuntos(conjunto_aux, conjuntos[pos]))
+
+def union_conjuntos(conjunto1: list, conjunto2: list) -> list:
+    for elemento1 in conjunto1:
+        if elemento1 not in conjunto2:
+            conjunto2.append(elemento1)
+
+    return conjunto2
